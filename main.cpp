5866451d98ef4285fec6b84e6c46a77e3508553c
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-01 21:22:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
+#include "lib.h"
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a=0;
+    cin >> a;
+    if(carattere(a) == false){
+        cout << "errore" << endl;
+    }else{
+        int conv=(int) a;
+        if (97 <= a){
+            conv = toupper(a);
+            cout << char(conv) << endl;
+        } else if (65 <= conv <= 90){
+            cout << char(tolower(a));
+        }
+    }
+    return 0;
 }
