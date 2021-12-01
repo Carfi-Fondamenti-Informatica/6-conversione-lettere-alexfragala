90c56f1a75576d038062d2eb42ae259c7344528e
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-01 21:22:34 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by Alex Fragalà on 01/12/21.
+//
 
+#ifndef ESERCITAZIONE6_CORRETTA_LIB_H
+#define ESERCITAZIONE6_CORRETTA_LIB_H
+char carattere (char a);
+#endif //ESERCITAZIONE6_CORRETTA_LIB_H
