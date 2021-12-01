b8cfb41fc8cd643f3880259586b8fe18d70a4ef8
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-01 21:22:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+//
+// Created by Alex Fragalà on 01/12/21.
+//
+#include <iostream>
+#include "lib.h"
 
+char carattere(char a){
+    if(isalpha(a)){
+        return true;
+    } else if (isdigit(a)){
+        return false;
+    }
+}
